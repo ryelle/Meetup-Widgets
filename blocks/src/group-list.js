1366fa4ebd8ccca09aca1388e74c2391e5d0570a
Kelly Dwan <ryelle@users.noreply.github.com>
2017-12-08 18:57:42 -0500

    Build out block controls

--- blocks/src/group-list.js
+++ blocks/src/group-list.js
@@ -1,8 +1,15 @@
 const { __ } = wp.i18n;
+const {
+	BlockDescription,
+	InspectorControls,
+	InspectorControls: { TextControl },
+} = wp.blocks;
+
+const translate = str => __(str, 'meetup-widgets');
 
 // Visit https://wordpress.org/gutenberg/handbook/block-api/ to learn about Block API
 export default {
-	title: __( 'Meetup.com List', 'meetup-widgets' ),
+	title: translate('Meetup.com List'),
 
 	icon: 'editor-ul',
 
@@ -11,11 +18,28 @@ export default {
 	// Remove to make block editable in HTML mode.
 	supportHTML: false,
 
-	edit: props => {
-		return __( 'Group list here! 🎊', 'meetup-widgets' );
+	edit: ({ attributes, setAttributes, focus, setFocus }) => {
+		console.log(attributes);
+		return [
+			<div className="">
+				<h2>{translate('Text!')}</h2>
+			</div>,
+			!!focus && (
+				<InspectorControls key="inspector">
+					<BlockDescription>
+						<h3>{translate('Meetup.com ?')}</h3>
+					</BlockDescription>
+					<TextControl
+						label={translate('Group')}
+						value={attributes.test}
+						onChange={value => setAttributes({ test: value })}
+					/>
+				</InspectorControls>
+			),
+		];
 	},
 
 	save: () => {
-		return null
-	}
+		return null;
+	},
 };
