213edcf2593f309d781da95d4ec22e73c9c37df2
Kelly Dwan <ryelle@users.noreply.github.com>
2017-12-25 10:16:55 -0500

    Add hot reloading

--- blocks/blocks.php
+++ blocks/blocks.php
@@ -11,13 +11,19 @@
  */
 function vs_meetup_widgets_enqueue_block_assets() {
 	$dir = dirname( __FILE__ );
-	wp_enqueue_script( 'group-list-block', plugins_url( 'build/index.js', __FILE__ ), array(
-		'wp-blocks',
-		'wp-i18n',
-		'wp-element',
-	), filemtime( "$dir/build/index.js" ) );
-	wp_enqueue_style( 'group-list-block', plugins_url( 'build/editor.css', __FILE__ ), array(
-		'wp-blocks',
-	), filemtime( "$dir/build/editor.css" ) );
+	$js_file = 'http://localhost:8081/blocks/build/index.js';
+	$css_file = 'http://localhost:8081/blocks/build/style.css';
+	$js_version = false;
+	$css_version = false;
+
+	if ( ! WP_DEBUG ) {
+		$js_file = plugins_url( 'build/index.js', __FILE__ );
+		$css_file = plugins_url( 'build/editor.css', __FILE__ );
+		$js_version = filemtime( "$dir/build/index.js" );
+		$css_version = filemtime( "$dir/build/editor.css" );
+	}
+
+	wp_enqueue_script( 'meetup-blocks', $js_file, [ 'wp-blocks', 'wp-i18n', 'wp-element' ], $js_version );
+	wp_enqueue_style( 'meetup-blocks', $css_file, [ 'wp-blocks' ], $css_version );
 }
 add_action( 'enqueue_block_editor_assets', 'vs_meetup_widgets_enqueue_block_assets' );
