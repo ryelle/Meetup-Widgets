2fbfd06d2a35dc223c0b47bb288b2b83c7db68ca
Kelly Dwan <ryelle@users.noreply.github.com>
2018-01-03 18:58:06 -0500

    Send build files to a folder in plugin root, ignore build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+/** @format */
+
+const webpack = require( 'webpack' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 
 // CSS loader for styles specific to blocks in general.
-const blocksCSSPlugin = new ExtractTextPlugin({
-	filename: './blocks/build/editor.css',
-});
+const blocksCSSPlugin = new ExtractTextPlugin( {
+	filename: './build/editor.css',
+} );
 
 // Configuration for the ExtractTextPlugin.
 const extractConfig = {
@@ -13,16 +15,16 @@ const extractConfig = {
 		{
 			loader: 'postcss-loader',
 			options: {
-				plugins: [require('autoprefixer')],
+				plugins: [ require( 'autoprefixer' ) ],
 			},
 		},
 	],
 };
 
 module.exports = {
-	entry: __dirname + '/blocks/src/index.js',
+	entry: __dirname + '/includes/blocks/src/index.js',
 	output: {
-		filename: 'blocks/build/index.js',
+		filename: 'build/index.js',
 		path: __dirname,
 	},
 	module: {
@@ -34,17 +36,15 @@ module.exports = {
 			},
 			{
 				test: /style\.css$/,
-				include: [/blocks/],
-				use: blocksCSSPlugin.extract(extractConfig),
+				include: [ /blocks/ ],
+				use: blocksCSSPlugin.extract( extractConfig ),
 			},
 		],
 	},
 	plugins: [
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(
-				process.env.NODE_ENV || 'development',
-			),
-		}),
+		new webpack.DefinePlugin( {
+			'process.env.NODE_ENV': JSON.stringify( process.env.NODE_ENV || 'development' ),
+		} ),
 		blocksCSSPlugin,
 	],
 	devServer: {
